69f28a5f97c0b7b81c61e5bad73b8b596c440f4a
SaqibShafiq007 <saqibshafiq618@gmail.com>
2026-03-08 06:47:08 +0500

    fix: updated cors and paths

--- Server/server.js
+++ Server/server.js
@@ -11,14 +11,34 @@ const port = process.env.PORT||4000
 connectDB()
 
 
-const allowedOrigin =['http://localhost:5173']
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: allowedOrigin , credentials:true}))
+
+const cors = require('cors');
+
+const allowedOrigins = [
+  'https://mern-auth-k9fo.vercel.app', // Your production URL
+  'http://localhost:5173'              // Your local development URL
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      return callback(new Error('CORS policy violation'), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
 
 app.get('/' ,(req,res )=> res.send("API working fin") );
 app.use('/api/auth' , authRouter)
 app.use('/api/user' , userRouter )
 
 app.listen(port , ()=> console.log(`Server started on port ${port}`));
+
+
+module.exports = app; //for vercel
